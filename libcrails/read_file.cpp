cbba76c09d07649d088838e6468bd136d7016755
plaristote <michael@unetresgrossebite.com>
2022-08-16 22:56:55 +0200

    remove unused variable in read_file.cpp

--- libcrails/read_file.cpp
+++ libcrails/read_file.cpp
@@ -11,14 +11,12 @@ bool Crails::read_file(const string& filepath, string& out)
   if (file.is_open())
   {
     long           size, begin, end;
-    char*          raw;
 
     begin       = file.tellg();
     file.seekg(0, std::ios::end);
     end         = file.tellg();
     file.seekg(0, std::ios::beg);
     size        = end - begin;
-    raw         = new char[size + 1];
     out.resize(size);
     file.read(&out[0], size);
     file.close();
