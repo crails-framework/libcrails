f3a642ce8b9d6cb9542e945658427fe38a4c7c96
plaristote <michael@unetresgrossebite.com>
2022-08-17 14:39:59 +0200

    update WebSocket

--- libcrails/websocket.cpp
+++ libcrails/websocket.cpp
@@ -1,9 +1,12 @@
+#include <boost/asio/dispatch.hpp>
 #include "websocket.hpp"
-#include <crails/connection.hpp>
+#include "server/connection.hpp"
+#include "logger.hpp"
 
 using namespace Crails;
 
-WebSocket::WebSocket(Connection& connection) : stream(std::move(connection.get_stream().socket()))
+WebSocket::WebSocket(Connection& connection) : stream(std::move(connection.get_stream().socket())),
+  dynamic_buffer(boost::asio::dynamic_buffer(receive_buffer))
 {
   receive_buffer.reserve(4096);
   send_buffer.reserve(4096);
@@ -18,10 +21,13 @@ void WebSocket::run()
 
 void WebSocket::read()
 {
-  stream.async_read(buffer, boost::beast::bind_front_handler(&WebSocket::on_read, shared_from_this()));
+  stream.async_read(
+    dynamic_buffer,
+    boost::beast::bind_front_handler(&WebSocket::on_read, shared_from_this())
+  );
 }
 
-void WebSocket::send(const std::string& message, MessageType type = TextMessage)
+void WebSocket::send(const std::string& message, MessageType type)
 {
   send_buffer = message;
   stream.text(type == TextMessage);
@@ -32,7 +38,7 @@ void WebSocket::send(const std::string& message, MessageType type = TextMessage)
   );
 }
 
-void WebSocket::send(const char* message, std::size_t length, MessageType type = TextMessage)
+void WebSocket::send(const char* message, std::size_t length, MessageType type)
 {
   send_buffer.reserve(length);
   for (std::size_t i = 0 ; i < length ; ++i)
@@ -58,26 +64,37 @@ void WebSocket::on_run()
 
 void WebSocket::on_accept(boost::beast::error_code ec)
 {
-  if (ec)
-    return ; // error
-  read();
+  if (!ec)
+  {
+    logger << Logger::Info << "websocket connection opened" << Logger::endl;
+    read();
+  }
+  else
+    logger << Logger::Error << "Could not accept websocket: " << ec.message() << Logger::endl;
 }
 
 void WebSocket::on_read(boost::beast::error_code ec, std::size_t)
 {
   if (ec == boost::beast::websocket::error::closed)
   {
+    logger << Logger::Info << "websocket connection closed" << Logger::endl;
     disconnected();
     return ;
   }
   if (ec)
+  {
+    logger << Logger::Error << "websocket read error: " << ec.message() << Logger::endl;
     return ; // error
+  }
   received(receive_buffer, stream.got_text() ? TextMessage : BinaryMessage);
 }
 
 void WebSocket::on_write(boost::beast::error_code ec, std::size_t)
 {
-  if (ec) // error
+  if (ec)
+  {
+    logger << Logger::Error << "websocket write error: " << ec.message() << Logger::endl;
     return ;
+  }
   read();
 }
