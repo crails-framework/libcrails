f35b1be84b55b6a827dc11c183b39ed2c5fa41b4
plaristote <michael@unetresgrossebite.com>
2022-09-21 02:51:55 +0200

    move default_to to any_cast

--- libcrails/crails/any_cast.hpp
+++ libcrails/crails/any_cast.hpp
@@ -7,6 +7,16 @@
 namespace Crails
 {
   std::string any_cast(const boost::any& val);
+
+  template<typename V>
+  static V defaults_to(const std::map<std::string, boost::any>& a, const std::string& k, const V def)
+  {
+    typename std::map<std::string, boost::any>::const_iterator it = a.find(k);
+
+    if (it == a.end())
+      return def;
+    return boost::any_cast<V>(it->second);
+  }
 }
 
 #endif
