4ef42f9e287f69e93ebb070bcd9a01d3dda3a5c0
plaristote <michael@unetresgrossebite.com>
2024-01-03 18:32:11 +0100

    Crails::any_cast now supports std::string_view as well

--- libcrails/crails/any_cast.cpp
+++ libcrails/crails/any_cast.cpp
@@ -7,6 +7,8 @@ namespace Crails
   {
     if (typeid(const char*) == val.type())
       return std::string(boost::any_cast<const char*>(val));
+    if (typeid(std::string_view) == val.type())
+      return std::string(boost::any_cast<std::string_view>(val));
     return boost::any_cast<std::string>(val);
   }
 }
