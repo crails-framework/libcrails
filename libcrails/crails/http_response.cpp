733048ed60619a31961d5fb73974c11e01d8a0c2
plaristote <michael@unetresgrossebite.com>
2026-06-30 23:50:31 +0200

    guard HttpResponse against modifying response while its being sent

--- libcrails/crails/http_response.cpp
+++ libcrails/crails/http_response.cpp
@@ -6,18 +6,19 @@ using namespace Crails;
 
 void BuildingResponse::set_response(HttpStatus code, string&& body)
 {
-  set_status_code(code);
   set_body(std::move(body));
+  set_status_code(code);
 }
 
 void BuildingResponse::set_response(HttpStatus code, const string_view body)
 {
-  set_status_code(code);
   set_body(body.data(), body.length());
+  set_status_code(code);
 }
 
 void BuildingResponse::set_body(const char* str, size_t size)
 {
+  if (sent()) return ;
   auto&   response = get_raw_response();
 
   response.body().assign(str, size);
@@ -26,6 +27,7 @@ void BuildingResponse::set_body(const char* str, size_t size)
 
 void BuildingResponse::set_body(string&& value)
 {
+  if (sent()) return ;
   auto&   response = get_raw_response();
 
   response.body() = std::move(value);
