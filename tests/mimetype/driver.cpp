0d4db4b0c49454d2981a21a43d1d653560e65188
plaristote <michael@unetresgrossebite.com>
2026-09-08 20:49:47 +0200

    stricter mimetype checks

--- tests/mimetype/driver.cpp
+++ tests/mimetype/driver.cpp
@@ -9,7 +9,7 @@ int main()
   using namespace std;
 
   assert(get_mimetype("file.html") == "text/html");
-  assert(get_mimetype("PICTURE.JPEG") == "image/jpg");
+  assert(get_mimetype("PICTURE.JPEG") == "image/jpeg");
   assert(get_mimetype("Music.Opus") == "audio/ogg");
   assert(get_mimetype("index.html.gz") == "text/html");
   assert(get_mimetype("document.pDF") == "application/pdf");
